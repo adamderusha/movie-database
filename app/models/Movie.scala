f3680808486f4f45d8607aceb452c25978bc85de
Adam DeRusha <apd3691@gmail.com>
2013-03-05 01:13:51 -0500

    Got model working (inefficiently)

--- app/models/Movie.scala
+++ app/models/Movie.scala
@@ -8,20 +8,29 @@ import play.api.Play.current
 case class Movie(id:     Long,
                  title:  String,
                  year:   Int,
-                 poster: String)
+                 poster: String,
+                 genres: List[String])
 
 object Movie {
-  val movie = {
+  val multiparser = {
     get[Long]("id") ~
     get[String]("title") ~
     get[Int]("year") ~
-    get[String]("poster") map {
-      case id~title~year~poster => Movie(id, title, year, poster)
+    get[String]("poster") ~ 
+    get[String]("genre") map {
+      case id~title~year~poster~genre => (id, title, year, poster, genre)
     }
   }
 
   def all(): List[Movie] = DB.withConnection { implicit c =>
-    SQL("SELECT * FROM movie ORDER BY title ASC").as(movie *)
+    val rows = SQL("""
+                   SELECT m.id, m.title, m.year, m.poster, g.genre
+                   FROM movie m, genre g 
+                   WHERE m.id = g.movie_id
+                   ORDER BY title ASC
+                   """).as(multiparser *)
+    val data = rows.map(r => (r._2,r._5)).groupBy(_._1).mapValues { l => l.map(_._2) }
+    rows.map(r => Movie(r._1, r._2, r._3, r._4, data(r._2))).distinct
   }
 }
 
