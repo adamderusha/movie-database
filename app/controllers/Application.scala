4490c05288e3d80d8859f554570ae3baad657013
Adam DeRusha <apd3691@gmail.com>
2013-03-18 16:35:23 -0400

    Started work on the lookup movie functionality.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,6 +3,9 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import play.api.data._
+import play.api.data.Forms._
+
 import models.Movie
 
 object Application extends Controller {
@@ -10,5 +13,15 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index(Movie.all()))
   }
+
+  def lookupPage = Action {
+    Ok(views.html.lookup())
+  }
+
+  def lookupAction = Action {
+    Redirect(routes.Application.index)
+  }
+
+  def add = TODO
   
 }
