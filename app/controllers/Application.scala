a1c07dbef8195444406b4bf8da0c4aadc0e7e8ea
Adam DeRusha <apd3691@gmail.com>
2013-03-20 02:54:46 -0400

    Refactored and commented for a bit of a cleaner look

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,59 +10,26 @@ import play.api.data.Forms._
 
 import play.api.Logger
 
-import play.api.libs.ws._
-import scala.concurrent._
-import scala.concurrent.duration._
-import play.api.libs.concurrent.Execution.Implicits._
-import play.api.libs.json._
-
 import models.Movie
+import utility.DataGrabber
 
 object Application extends Controller {
   val api_key = Play.current.configuration.getString("application.rotten_api_key") match {
     case Some(key) => key
     case None => {
-      Logger.warn("apikey not found!")
-      ""
+      Logger.error("apikey not found!")
+      //Should find a better way to handle this,
+      //but this shouldn't run if the key is missing
+      //since it's needed for functionality
+      System.exit(1) 
+      "" //To shut up the compiler...will never get here
     }
   }
 
-  def encode(url: String): String = java.net.URLEncoder.encode(url, "UTF-8")
-
-  def getMovieList(title: String, page_limit: Int = 10): List[Movie] = {
-    val url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json"
-    val parameters = "apikey=%s&q=%s&page_limit=%d".format(api_key, encode(title), page_limit)
-    val finalUrl = url + "?" + parameters
-    Logger.debug(finalUrl)
-    val jsonData = WS.url(finalUrl).get
-    val movieData: Future[List[Movie]] = jsonData.flatMap { response =>
-      val json = response.json
-      val moviesTemp = (json \ "movies").as[List[JsValue]].map { x => 
-        val title = (x \ "title").as[String]
-        val year = (x \ "year").as[Int]
-        val poster = (x \ "posters" \ "original").as[String]
-        val link = (x \ "links" \ "self").as[String]
-        (title, year, poster, link)
-      }.toList
-      val result: List[Future[Movie]] = moviesTemp.map { t =>
-        val url = "%s?apikey=%s".format(t._4, api_key)
-        val genreData = WS.url(url).get
-        genreData.map { genre =>
-          val genreJson = genre.json
-          val genres = (genreJson \ "genres").as[List[String]]
-          t match {
-            case (t, y,p, _) => Movie(t,y,p,genres)
-          }
-        }
-      }
-      Future.sequence(result)
-    }
-    
-    Await.result(movieData, 3 second).asInstanceOf[List[Movie]]
-  }
 
   def index = Action {
-    getMovieList("Toy Story")
+    val getter = new DataGrabber(api_key)
+    val movies = getter.getMovieList("Toy Story")
     Ok(views.html.index(Movie.all()))
   }
 
---
+++ app/utility/DataGrabber.scala
@@ -0,0 +1,71 @@
+package utility
+
+import play.api.libs.ws._
+import scala.concurrent._
+import scala.concurrent.duration._
+import play.api.libs.concurrent.Execution.Implicits._
+import play.api.libs.json._
+
+import models.Movie
+
+class DataGrabber(api_key: String) {
+  // Used to URL Encode a string
+  private def encode(url: String): String = java.net.URLEncoder.encode(url, "UTF-8")
+
+  // Takes a movie JSON value and extracts the relevent data from it
+  private def generateMovieData(data: JsValue)
+    : (String, Int, String, String) = {
+    val title = (data \ "title").as[String]
+    val year = (data \ "year").as[Int]
+    val poster = (data \ "posters" \ "original").as[String]
+    val link = (data \ "links" \ "self").as[String]
+    (title, year, poster, link)
+  }
+
+  // Builds a url like http://www.site.com/api?param1=value1&param2=value2 etc.
+  private def urlBuilder(base: String, params: Map[String, String]): String = {
+    val paramString = params.map(t => "%s=%s".format(t._1, t._2)).mkString("&")
+    "%s?%s".format(base, paramString)
+  }
+
+  def getMovieList(title: String, page_limit: Int = 10): List[Movie] = {
+    //Get movie data from rottentomatoes api
+    val movieUrl = "http://api.rottentomatoes.com/api/public/v1.0/movies.json"
+    val movieParams = Map(
+      "apikey" -> api_key,
+      "page_limit" -> page_limit.toString,
+      "q" -> encode(title)
+    )
+    val url = urlBuilder(movieUrl, movieParams)
+    val jsonData = WS.url(url).get
+
+    //Now to go through the JSON returned.
+    val movies: Future[List[Movie]] = jsonData.flatMap { response =>
+      val movieJson = response.json
+
+      // This will take the JSON, make a list of the movies JSON, then transform it into a tuple of data needed
+      val rawMovieData = (movieJson \ "movies").as[List[JsValue]].map(generateMovieData(_)).toList
+
+      // For each movie, request from the api more info (we need the genres)
+      val result: List[Future[Movie]] = rawMovieData.map { t =>
+        val moreMovieInfoUrl = urlBuilder(t._4, Map("apikey" -> api_key))
+        val genreData = WS.url(moreMovieInfoUrl).get
+
+        // Go through the JSON, extract the genres, and create the Movie class
+        genreData.map { genre =>
+          val genreJson = genre.json
+          val genres = (genreJson \ "genres").as[List[String]]
+          t match {
+            case (t, y, p, _) => Movie(t, y, p, genres)
+          }
+        }
+      }
+
+      // Finally convert the result into a Future[List of movies]
+      Future.sequence(result)
+    }
+   
+    // Wait for the data to come in
+    Await.result(movies, 5 second).asInstanceOf[List[Movie]]
+  }
+}
