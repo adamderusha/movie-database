6fcd2d378c9cb4a0c81889916ccc6a732a7dd1f7
Adam DeRusha <apd3691@gmail.com>
2013-03-29 13:22:55 -0400

    Added some error handling.

--- app/utility/DataGrabber.scala
+++ app/utility/DataGrabber.scala
@@ -17,11 +17,18 @@ class DataGrabber(api_key: String) {
   // Takes a movie JSON value and extracts the relevent data from it
   private def generateMovieData(data: JsValue)
     : (String, Int, String, String) = {
-    val title = (data \ "title").as[String]
-    val year = (data \ "year").as[Int]
-    val poster = (data \ "posters" \ "profile").as[String]
-    val link = (data \ "links" \ "self").as[String]
-    (title, year, poster, link)
+    try {
+      val title = (data \ "title").as[String]
+      val year = (data \ "year").as[Int]
+      val poster = (data \ "posters" \ "profile").as[String]
+      val link = (data \ "links" \ "self").as[String]
+      (title, year, poster, link)
+    } catch {
+      case js: Exception => { 
+        Logger.warn("generateMovieData - Could not parse the following JSON: " + Json.stringify(data))
+        ("", 0, "","")
+      }
+    }
   }
 
   // Builds a url like http://www.site.com/api?param1=value1&param2=value2 etc.
@@ -41,15 +48,19 @@ class DataGrabber(api_key: String) {
     val url = urlBuilder(movieUrl, movieParams)
     val jsonData = WS.url(url).get
 
-    //Now to go through the JSON returned.
+    // Now to go through the JSON returned.
     val movies: Future[List[Movie]] = jsonData.flatMap { response =>
       val movieJson = response.json
 
       // This will take the JSON, make a list of the movies JSON, then transform it into a tuple of data needed
       val rawMovieData = (movieJson \ "movies").as[List[JsValue]].map(generateMovieData(_)).toList
 
+      // Any errors in data get filtered out (ie missing a year value).
+      val filteredMovieData = rawMovieData.filter(t => !t._1.isEmpty)
+
+
       // For each movie, request from the api more info (we need the genres)
-      val result: List[Future[Movie]] = rawMovieData.map { t =>
+      val result: List[Future[Movie]] = filteredMovieData.map { t =>
         val moreMovieInfoUrl = urlBuilder(t._4, Map("apikey" -> api_key))
         val genreData = WS.url(moreMovieInfoUrl).get
 
